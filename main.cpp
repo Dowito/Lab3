597c385bdf0d9e054b899bba7093bfbffcf88b07
David <david.gonzalez12@gmail.com>
2022-02-12 00:12:38 -0500

    Se hicieron pruebas a getBits. Funciona bien.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ int main()
     char cad[] = "../Lab3/Data_base/natural.txt";
     char *cadArchivo = rArchivo(cad);
     cout << *cadArchivo << endl;
-    char arr[4] = {'A','b','C','d'};
+    char arr[5] = {'A','b','C','d','\0'};
+    bool *bits = getBits(arr);
+    bool *pruebaBits = getBits(cadArchivo);
+    unsigned long long tam = lenCad(cadArchivo);
+    for (unsigned long long i=0; i<8*tam; i++) {
+        cout << pruebaBits[i];
+    }
+
     return 0;
 }
