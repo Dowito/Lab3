34832b8c43633421987a782f012873c0e3195557
David <david.gonzalez12@gmail.com>
2022-02-11 20:09:37 -0500

    Creacion rama desarollo_metodo1.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 //nombre archivo sin codificar y codificado natural.txt
 //codificado codificado.dat
+//Creacion rama desarrollo metodo1
 int main()
 {
     cout << "Hello World!" << endl;
