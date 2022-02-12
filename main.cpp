feffe3d24e9f4003d6456cc6d3819b832aa49681
David <david.gonzalez12@gmail.com>
2022-02-11 22:29:10 -0500

    Chulea tarea, leer archivo y guardarlo en una cadena.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <manejoArchivo.h>
 
-//nombre archivo sin codificar y codificado natural.txt
-//codificado codificado.dat
-//Creacion rama desarrollo metodo1
+//lacad que returna rArchivo lleva un '\0' no se que problemas me genero eso
 
 int main()
 {
     char cad[] = "../Lab3/Data_base/natural.txt";
     char *cadArchivo = rArchivo(cad);
+    cout << *cadArchivo;
     return 0;
 }
