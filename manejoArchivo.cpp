006091227cc54ba3b90b1ed3d18da3bf859003c4
David <david.gonzalez12@gmail.com>
2022-02-11 21:48:05 -0500

    Agregada funcion rArchivo. No se han hecho pruebas.

--- manejoArchivo.cpp
+++ manejoArchivo.cpp
@@ -1,7 +1,20 @@
 #include <manejoArchivo.h>
 
-char *rArchivo(char *cad)
+char *rArchivo(char *direccion)
 {
-    fstream archivo;
-    archivo.open(cad, fstream::out | fstream::binary);
+    fstream Archivo;
+    Archivo.open(direccion, fstream::in | fstream::ate);  //Mode oout->Lectura  ate->abre poniendo el cursor en el ultimo archivo
+    if(Archivo.is_open()){ //Procedemos a guardar el archivo en un arreglo de char
+        unsigned long long tam = Archivo.tellg();//Retorna la ultima posicion del archivo
+        Archivo.seekg(0); //curso en 0
+        char *cad = new char [tam] {'\0'}; //Reservamos suficiente espacio para guardar el archivo en el heap
+        Archivo.read(cad, tam); //cad guarda el archivo
+        Archivo.close();
+        return cad;
+    }
+    else{
+        cout << "No se pudo leer el archivo.";
+        char cad[] = "Error";
+        return cad;
+    }
 }
