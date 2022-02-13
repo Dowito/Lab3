1220716552b17966a4bd6e6529fd1944c269cb1b
David <david.gonzalez12@gmail.com>
2022-02-12 19:10:18 -0500

    Creacion Rama PruebasMetodo1.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <manejoArchivo.h>
 
 //lacad que returna rArchivo lleva un '\0' no se que problemas me genero eso
-
 int main()
 {
+    //Pruebas intensivas :)
     unsigned long long semilla = 4;
     char path[] = "../Lab3/Data_base/natural.txt";
     char EjGuia[5] = {'A','b','C','d','\0'};
